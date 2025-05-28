d123f10a4bd0654efcf6317c68e7c20ba4bde507
YuriSolomon <juristein21@gmail.com>
2025-05-28 13:42:08 +0200

    complete quiz and shuffle answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
@@ -13,12 +16,22 @@ export default function Quiz() {
         });
     };
 
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={quizCompleteImg} alt="Trophy icon" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+    
+    const shuffeledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffeledAnswers.sort(() => Math.random() - 0.5); //Math.random() gives a value between 0 and 1, reducing 0.5 will give a 50% chance for a negative number
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    {shuffeledAnswers.map(answer => (
                         <li className="answer" key={answer}>
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
