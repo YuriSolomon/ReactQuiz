ee984bbd984aa23c385e883d058a017ceae5a218
YuriSolomon <juristein21@gmail.com>
2025-05-30 15:05:31 +0200

    add correct and wrong answer indicator and css

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,17 +5,31 @@ import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
+    const [answerState, setAnswerState] = useState('');
     const [userAnswers, setUserAnswers] = useState([]);
-
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    const handleSelectAnswer =  useCallback(function handleSelectAnswer(selectedAnswer) { //use callback to run the function once for depandancy of handleSkipAnswer. depandancy is not needed.
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) { //use callback to run the function once for depandancy of handleSkipAnswer. depandancy is not needed.
+        setAnswerState('answered');
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
-    }, []);
+
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct');
+            } else {
+                setAnswerState('wrong');
+            }
+
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000)
+        }, 1000);
+
+    }, [activeQuestionIndex]); // recreate the method every time the answer index changes.
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]); //creates a callback to prevent useEffect depandancy from being called on function. add handleSelectAnswer as a depandancy because it is affected by props and might run on props change
 
@@ -25,7 +39,7 @@ export default function Quiz() {
             <h2>Quiz Completed!</h2>
         </div>
     }
-    
+
     const shuffeledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
     shuffeledAnswers.sort(() => Math.random() - 0.5); //Math.random() gives a value between 0 and 1, reducing 0.5 will give a 50% chance for a negative number
 
@@ -36,11 +50,24 @@ export default function Quiz() {
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffeledAnswers.map(answer => (
-                        <li className="answer" key={answer}>
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))}
+                    {shuffeledAnswers.map(answer => {
+                        const isSelected = userAnswers[userAnswers.length - 1] === answer;
+                        let cssClass = '';
+
+                        if (answerState === 'answered' && isSelected) {
+                            cssClass = 'selected'
+                        }
+
+                        if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                            cssClass = answerState;
+                        }
+
+                        return (
+                            <li className="answer" key={answer}>
+                                <button className={cssClass} onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         </div>
